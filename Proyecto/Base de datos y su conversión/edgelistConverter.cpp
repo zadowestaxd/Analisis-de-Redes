796f80b6908363d743f7cca090648ff37a11c2e5
Alejandro Valencia Blancas <alezombi@gmail.com>
2022-12-25 14:37:25 +0100

    Actualización NAVIDAD
    últimos retoques base de datos

--- Proyecto/Base de datos y su conversión/edgelistConverter.cpp
+++ Proyecto/Base de datos y su conversión/edgelistConverter.cpp
@@ -5,10 +5,9 @@
 #include<vector>
 using namespace std;
 
-const int POKEMONS = 214;
-const pair<int, int> INTERVALO_EP = { 212, 277 };
-const int POPULARES = 2;
-
+const int POKEMONS = 214; //segunda temporada 168 pokemons, quinta temporada 214
+const int POPULARES = 2; //segunda temporada 3 popoulares, quinta temporada 2
+const int num_episodios = 64; //segunda temporada 63 episodios, quinta temporada 65
 int main() {
 	string temp;
 	int matriz[POKEMONS][POKEMONS] = {0};
@@ -33,6 +32,7 @@ int main() {
 		getline(archivo, temp);
 		if (temp == "\tToda la temporada") {
 			populares[numpopulares] = actual;
+			cuantoseps[actual] = num_episodios;
 			numpopulares++;
 		}else{
 			//va llenando el mapa y la matriz
@@ -58,8 +58,9 @@ int main() {
 		//mapa cuya key sea episodio y dentro contenga los pokemons. uniones de nodos
 	}
 	//Controla los pokemons con todos los episodios
-	for (int i = 0; i < POPULARES; i++) {
+	for (int i = 0; i < POPULARES; i++) {		
 		for (int j = 0; j < POKEMONS; j++) {
+			if(j != populares[i] && matriz[j][populares[i]] != num_episodios)
 			matriz[populares[i]][j] = cuantoseps[j];
 		}
 	}
@@ -67,10 +68,29 @@ int main() {
 	for (int i = 0; i < POKEMONS; i++) {
 		for (int j = 0; j < POKEMONS; j++) {
 			if (matriz[i][j] != 0) {
-				edgelist << i<<" "<<j << " "<< matriz[i][j] << "\n";
+				edgelist << i << " " << j << " " << matriz[i][j] << "\n";
 			}
 		}
 	}
+
 	cout << "EXITO AL CONVERTIR LA BASE DE DATOS A edgelist.csv"<<"\n"<<"\n";
+	
+	//Contador de episodios y pokemons que aparecen en estos	
+	auto it = diccionario.begin();
+	int cont = 0;
+	while (it != diccionario.end()){
+		cont++;
+		cout << "KEY/EPISODIO: " << it->first << " Pokemons que aparecen en este episodio: ";
+		auto it2 = diccionario[it->first].begin();
+		while (it2 != diccionario[it->first].end()) {
+			cout << it2->first<<" ";
+			it2++;
+		}
+		it++;
+		cout << endl;
+	}
+	//contador episodios
+	cout << cont;
+	
 	return 0;
 }
